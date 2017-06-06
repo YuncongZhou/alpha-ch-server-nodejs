9c780c1e3e27e5d9c7318ac67aeb0ecf63f804c4
Puru12 <zhouyuncong@gmail.com>
2017-06-05 21:41:55 -0400

    Create app.js

---
+++ backend/test/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/hello',function(req, res) {
+    res.send('hahaha')
+})
+
+app.post('/post', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
