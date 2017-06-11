2e9e5805c43c2fbc79f7539307c45aac45a70e71
Puru12 <zhouyuncong@gmail.com>
2017-06-11 00:06:37 -0400

    prototype

--- backend/test/prototype.js
+++ backend/test/prototype.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.json())  
-
 const MongoClient = require('mongodb').MongoClient
-const url = "mongodb://puru:19880818@ds155201.mlab.com:55201/test_new_mongodb"
+
+const app = express()
+app.use(bodyParser.json())
+
+const url = 'mongodb://puru:19880818@ds155201.mlab.com:55201/test_new_mongodb'
 
 app.listen(3000, () => console.log('Listening to port 3000'))
 app.get('/user/:id', (req, res) => res.send(`Welcome to the homepage of user ${req.params.id}.`))
 
-MongoClient.connect(url, function(err, db){  //it shows that the complexity of this block is 5, and it need some work on performance improvement
-    if(err) throw err
-    app.post('/post', function(req, res){
-        const data = req.body
-        console.log(data)
-        db.collection('News', (err, collection) => collection.insert(data))
-        res.send(`Success`)
-    }) 
-})
+const main = async () => {
+  const db = await MongoClient.connect(url)
+  app.post('/news', async (req, res) => {
+    const data = req.body
+    data.timestamp = new Date()
+    console.log(data)
+    const r = await db.collection('News').insertOne(data)
+    res.send('Success')
+  })
+}
+
+main()
