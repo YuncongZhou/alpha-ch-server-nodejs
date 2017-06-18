364bbdd334b3921cf0bdf3574affa91098ea260e
Puru12 <zhouyuncong@gmail.com>
2017-06-18 16:15:19 -0400

    comment insertion
    fixed some safty issues
    comment insertion implemented

--- backend/test/prototype.js
+++ backend/test/prototype.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,24 +15,49 @@ const main = async () => {
   const db = await MongoClient.connect(url)
   // post news and comment
   app.post('/data', async (req, res) => {
-    const data = req.body
+    const data = {}
+    data.type = req.body.type
+    data.url = req.body.url
+    data.body = req.body.body
     data.timestamp = new Date()
     data.upvote = 0
     data.downvote = 0
-    data.comments = {}
-    console.log(data)
-    const r = await db.collection('Data').insertOne(data)
+    data.comment_ids = []
     if (data.type === 'news') {
-      console.log('News added to the database.')
+      if (!await db.collection('Data').findOne({ url: data.url })) {
+        const r = await db.collection('Data').insertOne(data)
+        console.log('News added to the database.')
+        res.status(201).json({ success: true })
+      } else {
+        console.log('Duplicate news found. Abort.')
+        res.status(409).json({ success: false })
+      }
     } else {
-      console.log('Comment added to the database.') // Updating comment array is under development
+      const parentId = ObjectID.createFromHexString(req.body.parent_id, (err, r) => {
+        if (err) {
+          console.log('Parent post not found. Abort.')
+          res.status(409).json({ success: false })
+        }
+      })
+      if (parentId && await db.collection('Data').findOne({ _id: parentId })) {
+        const r = await db.collection('Data').insertOne(data)
+        await db.collection('Data').findOneAndUpdate(
+          { _id: parentId },
+          { $push: { comment_ids: r.insertedId.toHexString() } },
+          (err, r) => { console.log('A error occurred while inserting comment') })
+        console.log(r.insertedId.toHexString())
+        console.log('Comment added to the database.')
+        res.status(201).json({ success: true })
+      } else {
+        console.log('Parent post not found. Abort.')
+        res.status(409).json({ success: false })
+      }
     }
-    res.send('Success')
   })
   // retreive news and comment sorted by timestamp in reversed order
   app.get('/data', async (req, res) => {
     const timeline = await db.collection('Data').find().sort({ timestamp: -1 }).toArray()
-    res.send(timeline)
+    res.json(timeline)
   })
 }
 
