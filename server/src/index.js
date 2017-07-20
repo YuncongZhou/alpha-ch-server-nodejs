0f65a3bbfb9030d41a0d6a8188348a0f65370b83
Puru12 <zhouyuncong@gmail.com>
2017-07-20 18:23:46 -0400

    Add switch block and function
    Add a post creating function
    Use switch to choose news and two kinds of comments
    Change the fields in post

--- server/src/index.js
+++ server/src/index.js
@@ -11,52 +11,83 @@ const url = process.env.URL
 app.listen(3000, () => console.log('Listening to port 3000'))
 app.get('/user/:id', (req, res) => res.send(`Welcome to the homepage of user ${req.params.id}.`))
 
+function createPost(req, type) {
+  const post = {}
+  switch (type) {
+    case 0:
+      post.title = req.body.title
+      post.url = req.body.url
+      break
+    case 1:
+      post.title = req.body.title
+      post.body = req.body.body
+      break
+    case 2:
+      post.body = req.body.body
+      post.parent_id = req.body.parentId
+      break
+    default:
+      break
+  }
+  post.type = req.body.type
+  post.timestamp = new Date()
+  post.child_ids = []
+  post.upvote = 0
+  post.downvote = 0
+  return post
+}
+
 const main = async () => {
   const db = await MongoClient.connect(url)
-  // post news and comment
-  app.post('/data', async (req, res) => {
-    const data = {}
-    data.type = req.body.type
-    data.url = req.body.url
-    data.body = req.body.body
-    data.timestamp = new Date()
-    data.upvote = 0
-    data.downvote = 0
-    data.comment_ids = []
-    if (data.type === 'news') {
-      if (!await db.collection('Data').findOne({ url: data.url })) {
-        const r = await db.collection('Data').insertOne(data)
-        const id = r.insertedId.toHexString()
-        res.status(201).send({ post_id: id })
-      } else {
-        res.sendStatus(409)
-      }
-    } else if (req.body.parentId === null) {
-      const r = await db.collection('Data').insertOne(data)
-      const id = r.insertedId.toHexString()
-      res.status(201).send({ post_id: id })
-    } else {
-      try {
-        const parentId = ObjectID.createFromHexString(req.body.parentId)
-        if (await db.collection('Data').findOne({ _id: parentId })) {
-          const r = await db.collection('Data').insertOne(data)
+  // post case 0: news , case 1: top-level comment, case 2: non top-level comment
+  app.post('/posts', async (req, res) => {
+    switch (req.body.type) {
+      case 0: {
+        const duplicate = await db.collection('posts').findOne({ url: req.body.url })
+        if (!duplicate) {
+          const post = createPost(req, req.body.type)
+          const r = await db.collection('posts').insertOne(post)
           const id = r.insertedId.toHexString()
-          // const id = await insertToDatabase(db, data)
-          await db.collection('Data').updateOne(
-            { _id: parentId },
-            { $push: { comment_ids: id } })
-          res.status(201).send({ post_id: id })
+          res.status(201).send({ postId: id })
         } else {
+          res.status(409).send({ postId: duplicate._id.toHexString() })
+        }
+        break
+      }
+      case 1: {
+        const post = createPost(req, req.body.type)
+        const r = await db.collection('posts').insertOne(post)
+        const id = r.insertedId.toHexString()
+        res.status(201).send({ postId: id })
+        break
+      }
+      case 2: {
+        try {
+          const parentId = ObjectID.createFromHexString(req.body.parentId)
+          if (await db.collection('posts').findOne({ _id: parentId })) {
+            const post = createPost(req, req.body.type)
+            const r = await db.collection('posts').insertOne(post)
+            const id = r.insertedId.toHexString()
+            await db.collection('posts').updateOne(
+              { _id: parentId },
+              { $push: { child_ids: id } })
+            res.status(201).send({ postId: id })
+          } else {
+            res.sendStatus(400)
+          }
+        } catch (err) {
           res.sendStatus(400)
         }
-      } catch (err) {
-        res.sendStatus(400)
+        break
       }
+      default:
+        res.sendStatus(400)
+        break
     }
   })
   // retreive news and comment sorted by timestamp in reversed order
-  app.get('/data', async (req, res) => {
-    const timeline = await db.collection('Data').find().sort({ timestamp: -1 }).toArray()
+  app.get('/posts', async (req, res) => {
+    const timeline = await db.collection('posts').find().sort({ timestamp: -1 }).toArray()
     res.json(timeline)
   })
 }
