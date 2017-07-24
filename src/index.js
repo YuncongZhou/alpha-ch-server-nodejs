82bf90ae2f5074a74ad9d0b5598256d6a75497f9
Puru12 <zhouyuncong@gmail.com>
2017-07-24 14:20:04 -0400

    fix bug

--- src/index.js
+++ src/index.js
@@ -78,6 +78,8 @@ const main = async () => {
     res.status(201).send({ postId: id })
   })
   //vote posts
+  app.put('/votes/:id', async (req,res) =>{
+    const id = req.params.id
     const direction = req.body.direction
     if (direction){
       await db.collection('posts').updateOne({ _id: id }, { $inc: { upvote: 1 } })
@@ -85,6 +87,7 @@ const main = async () => {
       await db.collection('posts').updateOne({ _id: id }, { $inc: { downvote: 1 } })
     }
     res.status(200).send({ postId: id})
+  })
   // retreive news and comment sorted by timestamp in reversed order
   app.get('/posts', async (req, res) => {
     const timeline = await db.collection('posts').find().sort({ timestamp: -1 }).toArray()
