2edfa1d636cd8d2331bb5875407764d92d6beeb8
Puru12 <zhouyuncong@gmail.com>
2017-07-29 20:10:14 -0400

    Change default to sort by score

--- src/index.js
+++ src/index.js
@@ -116,7 +116,7 @@ const main = async () => {
     res.status(201).send(post)
   })
   // retreive news and comment sorted by wilson score or timestamp in reversed order
-  app.get('/posts/sorted', async (req, res) => {
+  app.get('/posts', async (req, res) => {
     const wilsonScore = await db.collection('posts').find().sort({ wilson_score: -1 }).toArray()
     res.json(wilsonScore)
   })
